0328629969254600542ea08fe61a77dd889f5d3e
jguz17 <johnathanguzman17@gmail.com>
2020-10-15 13:04:32 -0500

    init

---
+++ src/components/layout/Logo.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react'
+
+const Logo = () => {
+    return (
+        <Fragment>
+            <img src={require('../../images/logo-white.png')}/>
+        </Fragment>
+    )
+}
+
+export default Logo
---
+++ src/components/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Logo from '../layout/Logo'
+
+const Home = () => {
+    return (
+        <div className='home-container'>
+            <Logo className='logo'/>
+        </div>
+    )
+}
+
+export default Home
