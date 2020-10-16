d3b4d5c31c28af54d5ab83088fd8a41ce30a407f
jguz17 <johnathanguzman17@gmail.com>
2020-10-15 23:23:33 -0500

    finished form popup

--- src/components/layout/MainFooter.js
+++ src/components/layout/MainFooter.js
@@ -1,10 +1,26 @@
 import React from 'react'
 
 const MainFooter = () => {
+
+    const onClick = () => {
+        const square = document.querySelector('#test')
+
+        if (square.className.includes('hidden')) {
+            square.className += 'animation-test notHidden'
+            document.querySelector('#form-container').style.display = 'block'
+
+            // square.style.opacity = '100%'
+            // square.style.transform = 'translateX(0) translateY(0)%'
+            // square.style.height = 'translateX(0) translateY(0)%'
+            // square.style.transform = 'translateX(0) translateY(0)%'
+
+        } 
+    }
+
     return (
         <div className='footer-container'>
-            <button>Get Started</button>
-            <p>Already a member? Sign in</p>
+            <button id='register'>Get Started</button>
+            <p onClick={onClick}>Already a member? Sign in</p>
         </div>
     )
 }
