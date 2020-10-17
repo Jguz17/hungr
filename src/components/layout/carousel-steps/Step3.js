9a2c0a9e1cecf25fc12b7801dae328f6b0a0d03a
jguz17 <johnathanguzman17@gmail.com>
2020-10-17 11:48:29 -0500

    setup complete. time for revisions

--- src/components/layout/carousel-steps/Step3.js
+++ src/components/layout/carousel-steps/Step3.js
@@ -4,12 +4,18 @@ class Step3 extends React.Component {
     constructor(props) {
         super(props);
       }
+
+    //   componentDidMount() {
+          
+    //   }
+
     render() {
       if (this.props.currentStep !== 3) {
         return null;
       } 
+      document.querySelector('#hidden-text').style.display = 'block'
       return(
-        <div>
+        <div className='step-3'>
             <h1>What's your email?</h1>
             <p>Your email is used for updates</p>
             <input type='phone' required placeholder='hangry@hungr.com'/>
--- src/components/layout/carousel-steps/Step4.js
+++ src/components/layout/carousel-steps/Step4.js
@@ -9,7 +9,7 @@ class Step4 extends React.Component {
         return null;
       } 
       return(
-        <div>
+        <div className='step-4'>
             <h1>What's your name?</h1>
             <p>Your email is used for updates</p>
             <input type='phone' required placeholder='hangry@hungr.com'/>
