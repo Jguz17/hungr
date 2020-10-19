2995e869fe9e036b1d777fa38999183202e8081a
jguz17 <johnathanguzman17@gmail.com>
2020-10-19 16:33:02 -0500

    fixed hyphen issue

--- src/components/layout/carousel-steps/Step1.js
+++ src/components/layout/carousel-steps/Step1.js
@@ -18,12 +18,31 @@ const Step1 = (props) => {
       props.something(phone)
     }
 
+
+
+    const phoneT = document.getElementById('phoneNumber');
+
+    if (phoneT) {
+      phoneT.addEventListener("keydown", (e) => {
+        if(e.key === "Backspace" || e.key === "Delete") return;
+        if(e.target.value.length === 3) {
+          phoneT.value = phoneT.value + "-";
+        }
+        if(e.target.value.length === 7) {
+          phoneT.value = phoneT.value + "-";
+        }
+    })
+
+    }
+
+    
+
       return(
         <div className='step-1'>
             <form>
                 <h1>Enter your mobile phone number</h1>
                 <p>We'll send you a verification code</p>
-                <input onChange={onChange} id='phoneNumber' type='tel' required placeholder='987-654-3210' maxLength='10'/>
+                <input onChange={onChange} id='phoneNumber' type='tel' required placeholder='987-654-3210' maxLength='12'/>
             </form>
             <div style={{height: '40px', position: 'relative'}}>
               <Alerts/>
