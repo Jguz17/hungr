772c887aab8b859f0fc3c43611fad442b41279c2
jguz17 <johnathanguzman17@gmail.com>
2020-10-25 16:05:31 -0500

    changed phone cond

--- src/components/layout/Carousel.js
+++ src/components/layout/Carousel.js
@@ -58,8 +58,8 @@ const Carousel = (props) => {
 
     const receiveSomething = (something) => {
 
-        if (disabledStatus === 'true' && document.querySelector('#phoneNumber')) {
-                if (document.querySelector('#phoneNumber').value.length >= 12 && props.step == 1) {
+        if (disabledStatus === 'true' && props.step == 1) {
+                if (document.querySelector('#phoneNumber').value.length >= 12 ) {
                     setDisabled('false')
                     document.querySelector('#continue-button').addEventListener('click', () => {
 
